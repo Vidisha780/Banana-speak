8e691fe215c526616559968629b2769660fdb718
Vidisha780 <72399141+Vidisha780@users.noreply.github.com>
2021-08-15 18:24:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+// Need a reference to the button written in html 
+// querySelector() selects and returns the first element which matches the query
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+// var serverURL = "https://api.funtranslations.com/translate/shakespeare.json?";
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json?";
+
+function getTranslationURL(text)
+{
+    return serverURL + "text=" + text; 
+}
+
+function errorHandler(error)
+{
+    console.log("error occured " + error)
+}
+
+function clickEventHandler()
+{
+    // console.log("Clicked!");
+    // console.log("input ", txtInput.value);   //txtInput.value -> taking input and console
+    // outputDiv.innerText = "ajhggfrgdfv " + txtInput.value;      //output  
+
+        var inputText = txtInput.value;     //taking input
+    
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(data => {
+            // var translatedText = data.contents.translated;
+            var translatedText = data.contents.translated;
+            outputDiv.innerText = translatedText;       //output
+        })
+        .catch(errorHandler)
+}
+
+
+// listen/get ready to respnd to an event
+// When ever someone clicks, perform this function(here, clickEventListener)
+btnTranslate.addEventListener("click", clickEventHandler)
